ba4b4b3a07056481715ad168780a8058077bbf81
shuruq alshammari <shuruqalsh@gmail.com>
2024-12-19 15:45:00 +0300

    19-12-2024

---
+++ موحدون/Card.swift
@@ -0,0 +1,24 @@
+//
+//  Card.swift
+//  موحدون
+//
+//  Created by shuruq alshammari on 18/06/1446 AH.
+//
+
+
+import SwiftData
+import Foundation
+
+@Model
+class Card: Identifiable {
+    var id: UUID
+    var emoji: String
+    var cardDescription: String
+    
+    init(emoji: String, cardDescription: String) {
+        self.id = UUID() // سيتم تعيين UUID تلقائيًا
+        self.emoji = emoji
+        self.cardDescription = cardDescription
+    }
+}
+
