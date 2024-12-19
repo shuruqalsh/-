44d655ab9f1f8c924090b19138d79d98c37cb094
shuruq alshammari <shuruqalsh@gmail.com>
2024-12-19 14:51:38 +0300

    Initial Commit

---
+++ موحدون/______App.swift
@@ -0,0 +1,32 @@
+//
+//  ______App.swift
+//  موحدون
+//
+//  Created by shuruq alshammari on 18/06/1446 AH.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ______App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
